e4d13a31c2b1ca00ff96518b8fed7bdabea4aa01
abidekaw <abideka80@gmail.com>
2022-07-19 22:21:26 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ['./src/**/*.{html,js}'],
+   theme: {
+      container: {
+         center: true,
+         padding: '16px',
+      },
+      extend: {
+         colors: {
+            metal: '#565584',
+            dark: 'rgb(15 23 42)',
+            secondary: 'rgb(100 116 139)',
+         },
+         screens: {
+            '2xl': '1320px',
+         },
+      },
+   },
+   plugins: [],
+};
