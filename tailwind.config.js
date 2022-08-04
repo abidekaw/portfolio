0a8294125727dbcb6250d5e25d9b936709c196c4
abidekaw <abideka80@gmail.com>
2022-08-04 19:00:50 +0700

    change main theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,9 @@ module.exports = {
          colors: {
             metal: '#565584',
             dark: 'rgb(15 23 42)',
+            primary: '#bada55',
             secondary: 'rgb(100 116 139)',
+            main: 'rgb(var(--main-theme) / <alpha-value>)',
          },
          screens: {
             '2xl': '1320px',
